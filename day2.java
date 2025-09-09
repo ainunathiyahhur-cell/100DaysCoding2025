6d52ea2ba71084a8429d3d5a8742843269b07640
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-09-09 19:27:02 +0800

    Create day2.java

---
+++ day2.java
@@ -0,0 +1,10 @@
+public class day2
+ public static void main(String[] args) {
+
+    System.out.println("athiyahhurainun");
+    System.out.print(18);
+
+    System.out.printf("nama:%s, umur:%d", "athiyah", 18);
+ }
+
+  }
