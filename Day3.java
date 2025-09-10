1836e2fb8ab0c53fd2b1656f0474726541adcadd
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-09-10 19:01:55 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,14 @@
+public class day3 {
+
+   /**
+    *@param args the command line arguments
+    */
+   public static void main(String[] args) {
+     // Menyimpan nama
+     String nama = "Athiyah";
+     // Variabel Nama
+
+    // Menampilkan pesan prodi
+    System.out.println("Status : mahasiswa informatika");
+  }
+}
