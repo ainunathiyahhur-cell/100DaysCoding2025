03a66a4e484b1e9d647be2586e5bd0a3eea3080e
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-09-10 20:46:01 +0800

    Update Day3.java

--- Day3.java
+++ Day3.java
@@ -4,16 +4,18 @@ public class day3 {
     *@param args the command line arguments
     */
    public static void main(String[] args) {
-     // Menyimpan nama
+     //Komentar untuk satu baris//
+     /*Komentar lebih dari satu baris*/
+     
      String nama = "Athiyah";
-     // Variabel Nama
+     //Menyumpan variabel Nama//
 
   /* Menampilkan output ke layar
            Nama yang sudah disimpan
            dalam variabel di atas */
 
     System.out.println("Nama : " + nama);
-    // Menampilkan pesan tambahan prodi
+    /*Pesan dibawa ini Menampilkan prodi dilayar*/
     System.out.println("Status : mahasiswa informatika");
   }
 }
