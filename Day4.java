36deb7c2edfbf71ec9d57903658af9e860a65e50
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-09-11 20:02:43 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,15 @@
+public class Day4 {
+    public static void main(String[] args) {
+        // \n untuk baris baru
+        System.out.println("Ini baris pertama!\nIni baris kedua.");
+
+        // \t untuk tab
+        System.out.println("Athiyah\t18 tahun\tMajene");
+
+        // \" untuk menampilkan tanda kutip ganda
+        System.out.println("Halo: \"Selamat malam semuanya!\"");
+
+        // \\ untuk menampilkan backslash
+        System.out.println("Folder saya ada di: C:\\Users\\Athiyah\\Documents");
+    }
+}
