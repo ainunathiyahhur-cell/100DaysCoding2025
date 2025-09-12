69b24a35e107945ca8a5e61f6b1ebb53899a3a32
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-09-13 06:20:17 +0800

    Update Day5.java

--- Day5.java
+++ Day5.java
@@ -1,14 +1,12 @@
-package javasay5
-
-public class day5 {
-    public static void main(String [] args) {
-        byte umur = 18
-        short Tahun = 2007
-        int password = 240428
-        long nomorRumah = 20
-         System.out.println("umur")
-         System.out.println("Tahun")
-         System.out.println("password")
-         System.out.println("nomorRumah")
-      }
-  }
+public class Codingday5 {
+public static void main(String[] args) {
+        byte umur = 18;
+        short tahun = 2007;
+        int tinggiBadan= 160;
+        long nomorRumah = 20;
+        System.out.println(umur);
+        System.out.println(tahun);
+        System.out.println(beratBadan);
+        System.out.println(nomorRumah);
+    }
+}
