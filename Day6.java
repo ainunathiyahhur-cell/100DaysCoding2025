91f6886492bd9af127676894ac3d86d38a8c5090
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-09-13 22:40:25 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,13 @@
+public class day6 {
+    public static void main(String[] args) {
+        
+        double tinggiBadan = 160.2;
+        System.out.println("tinggi badan : " + tinggiBadan + "cm"  );
+        
+        float beratBadan = 57.5f;
+        System.out.println("berat badan : " + beratBadan + "kg");
+          
+        
+    }
+    
+}
