6188a3e32a18c523275580b417d0d80e7982edba
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-09-14 20:41:52 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,18 @@
+public class Codingday7 {
+public static void main(String[] args) {
+        // tipe data char
+        char huruf = 'A';
+        char nomor = '3';
+
+        System.out.println("huruf: " + huruf);
+        System.out.println("nomor: " + nomor);
+       
+       
+        //tipe data boolean
+        boolean sukaOlahraga = true;
+        System.out.println("suka olahraga");
+        
+        boolean sukaAyam = false;
+        System.out.println("suka ayam");
+    }
+}
