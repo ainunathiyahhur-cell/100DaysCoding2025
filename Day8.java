a6f13feb2e04a5a066de6102caaba3d5d4fc3adf
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-09-15 22:30:55 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,12 @@
+public class Day8 {
+    public static void main(String[] args) {
+       
+        String lahir= "saya lahir dimajene";
+        String warnaFavorit = "saya suka warna hitam";
+
+        
+        System.out.println(lahir);
+        System.out.println(warnaFavorit);
+    }
+  
+}
