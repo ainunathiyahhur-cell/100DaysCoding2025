ff1006f3a90fa77e1d4ee43f8a46bbb194bc793a
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-09-17 22:55:49 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,14 @@
+public class Day10 {
+    public static void main(String[] args) {
+        // Membuat konstanta dengan kata kunci final
+        final double PI = 3.15;
+        final int bulanLahir= 3;
+        final String nama = "Athiyah Hur Ainun";
+
+        // Menampilkan konstanta
+        System.out.println("Nilai PI       : " + PI);
+        System.out.println("bulan lahir  : " + bulanLahir);
+        System.out.println("Nama pengguna  : " + nama);
+
+    }
+}
