88a4e6214a981affa8a733dbb85623ed1e4ceefd
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-09-20 17:42:24 +0800

    Create Day13.java

---
+++ Day13.java
@@ -0,0 +1,16 @@
+package lanjutanevaluasi;
+
+public class LanjutanEvaluasi {
+
+    public static void main(String[] args) {
+        String nama = "Budi";
+        int umur = 20;
+        String alamat = "jakarta";
+        
+        System.out.println("--- BIODATA ---");
+        System.out.println("nama:\t" + nama);
+        System.out.println("umur:\t" + umur);
+        System.out.println("alamat:\t" + alamat);
+    }
+    
+}
