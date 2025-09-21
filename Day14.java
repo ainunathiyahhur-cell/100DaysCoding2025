64f9afed6b3b727e7c409bc445b98dd5c615739b
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-09-21 21:38:08 +0800

    Update Day14.java

--- Day14.java
+++ Day14.java
@@ -4,14 +4,14 @@ public class Codingday14 {
         String strInt = "789";
         int angka = Integer.parseInt(strInt);
 
-        String strDouble = "99.99";
-        double pecahan = Double.parseDouble(strDouble);
+        String strLong = "123456789";
+        long angkaBesar = Long.parseLong(strLong);
 
         String strBoolean = "false";
         boolean kondisi = Boolean.parseBoolean(strBoolean);
 
         System.out.println("Angka : " + angka);
-        System.out.println("Pecahan : " + pecahan);
+        System.out.println("Angka : " + angkaBesar);
         System.out.println("Kondisi : " + kondisi);
     }
 }
