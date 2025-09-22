7a6f0118050bdd71cace46ab13f6ea23e198dc93
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-09-22 21:44:45 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,14 @@
+public class day15 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 7;
+
+        int hasilTambah = a + b;
+        int hasilKurang = a - b;
+
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Hasil penjumlahan = " + hasilTambah);
+        System.out.println("Hasil pengurangan = " + hasilKurang);
+    }
+}
