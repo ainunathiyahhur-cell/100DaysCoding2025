885902021f23a0729f2f36dd8561ec59c0067212
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-09-23 22:09:02 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,14 @@
+public class day16 {
+    public static void main(String[] args) {
+        int angka1 = 20;
+        int angka2 = 5;
+
+        int hasilPerkalian = angka1 * angka2;
+        int hasilPembagian = angka1 / angka2;
+
+        System.out.println("Angka pertama: " + angka1);
+        System.out.println("Angka kedua: " + angka2);
+        System.out.println("Hasil perkalian: " + hasilPerkalian);
+        System.out.println("Hasil pembagian: " + hasilPembagian);
+    }
+}
