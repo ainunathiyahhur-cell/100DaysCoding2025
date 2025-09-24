b561f8cd8f91be7d939fddafcc8555f898edcd6d
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-09-24 22:06:16 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,12 @@
+public class Day17 {
+    public static void main(String[] args) {
+        int angka1 = 17;
+        int angka2 = 5;
+
+        int hasil = angka1 % angka2;
+
+        System.out.println("Angka pertama: " + angka1);
+        System.out.println("Angka kedua: " + angka2);
+        System.out.println("Sisa bagi: " + hasil);
+    }
+}
