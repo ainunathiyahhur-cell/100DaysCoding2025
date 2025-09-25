5f07c97a2e2f7fb03c44d62ecab2d33a5537011e
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-09-25 21:33:02 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,9 @@
+public class KonversiKecilBesar {
+    public static void main(String[] args) {
+        byte kecil = 42;
+        double besar = kecil;
+
+        System.out.println("Nilai kecil: " + kecil);
+        System.out.println("Nilai besar: " + besar);
+    }
+}
