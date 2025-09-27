7d4e823fca73713c3b5aa268299e07d97e11e70d
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-09-27 22:15:00 +0800

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,21 @@
+public class PrimitifKeString5 {
+    public static void main(String[] args) {
+        int umur = 18;
+        float suhu = 28.5f;
+        double beratBadan = 56.7;
+        char grade = 'B';
+        boolean aktif = true;
+
+        String strUmur = umur + "";
+        String strBeratBadan = Long.toString(beratBadan);
+        String strSuhu = String.valueOf(suhu);
+        String strGrade = grade + "";
+        String strAktif = Boolean.toString(aktif);
+
+        System.out.println("Umur ke String: " + strUmur);
+        System.out.println("Berat Badan ke String: " + strBeratBadan);
+        System.out.println("Suhu ke String: " + strSuhu);
+        System.out.println("Grade ke String: " + strGrade);
+        System.out.println("Aktif ke String: " + strAktif);
+    }
+}
