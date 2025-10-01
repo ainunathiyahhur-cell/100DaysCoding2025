ad7c17b9eeeb046a436624431edaf3c98f327781
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-10-01 21:33:14 +0800

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LuasLingkaran {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jari-jari lingkaran: ");
+        double r = input.nextDouble();
+
+        // Rumus luas lingkaran: π × r × r
+        double luas = Math.PI * r * r;
+
+        System.out.println("Luas lingkaran dengan jari-jari " + r + " adalah: " + luas);
+
+    }
+}
