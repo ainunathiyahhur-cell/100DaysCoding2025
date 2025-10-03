1afaf45c76993c1c49bea74c59509172fec09740
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-10-03 22:46:59 +0800

    Update Day26.java

--- Day26.java
+++ Day26.java
@@ -1,17 +1,21 @@
 package days26;
 public class Days26 {
     public static void main(String[] args) {
-        int x = 8;
+        int x = 8; //nilai awal
         int y = 3;
 
         x += y;
         System.out.println("Setelah x += y, hasil = " + x);
+
         x -= y;
         System.out.println("Setelah x -= y, hasil = " + x);
+
         x *= y;
         System.out.println("Setelah x *= y, hasil = " + x);
+
         x /= y;
         System.out.println("Setelah x /= y, hasil = " + x);
+
         x %= y;
         System.out.println("Setelah x %= y, hasil = " + x);
 
