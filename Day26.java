340ba5751a9ea3b67c29f34c924f117fbbd2ee63
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-10-03 21:25:25 +0800

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,19 @@
+package days26;
+public class Days26 {
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 3;
+
+        x += y;
+        System.out.println("Setelah x += y, hasil = " + x);
+        x -= y;
+        System.out.println("Setelah x -= y, hasil = " + x);
+        x *= y;
+        System.out.println("Setelah x *= y, hasil = " + x);
+        x /= y;
+        System.out.println("Setelah x /= y, hasil = " + x);
+        x %= y;
+        System.out.println("Setelah x %= y, hasil = " + x);
+
+    }
+}
