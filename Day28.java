ea918b787f45d31f4cc459730a710dac365a230e
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-10-05 21:36:34 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class day28 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai a: ");
+        int a = input.nextInt();
+
+        System.out.print("Masukkan nilai b: ");
+        int b = input.nextInt();
+
+        System.out.println("a == b " + (a == b));
+        System.out.println("a != b? " + (a != b));
+
+    }
+}
