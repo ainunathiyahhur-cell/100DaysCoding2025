26dd5239c8cddbd850be2d1eebd32b5d923c2d2d
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-10-09 22:23:45 +0800

    Update Day32.java

--- Day32.java
+++ Day32.java
@@ -1,15 +1,12 @@
-import java.util.Scanner;
 public class day32 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        int a = 7;
+        int b = 12;
 
-        System.out.print("Masukkan angka: ");
-        int a = input.nextInt();
-        System.out.print("Masukkan angka: ");
-        int b = input.nextInt();
-
-        boolean hasil = (a > 10 || b > 10);
-
-        System.out.println("Apakah salah satu angka lebih dari 10? " + hasil);
+        if (a > 10 || b > 10) {
+            System.out.println("Salah satu benar");
+        } else {
+            System.out.println("Keduanya salah");
+        }
     }
 }
