cc2de135a6b61b6e0be9c2ba84cb3004891d20a0
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-10-10 22:12:01 +0800

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Day34 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan Umur: ");
+        byte umurSaya = n.nextByte();
+
+        if (umurSaya != 18) {
+            System.out.println("Umur saya bukan 18!");
+        } else {
+            System.out.println("Umur saya 18!");
+    }
+}
