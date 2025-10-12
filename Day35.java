de245b09415dc6ec0b25da3b25f754c76b68170f
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-10-12 22:05:05 +0800

    Create Day35.java

---
+++ Day35.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class day35 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = input.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Angka " + angka + " adalah genap.");
+        } else {
+            System.out.println("Angka " + angka + " adalah ganjil.");
+        }
+    }
+}
