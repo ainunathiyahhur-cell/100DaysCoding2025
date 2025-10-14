fca34c393934a4812246652c61381534e67ff26c
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-10-14 20:33:53 +0800

    Create Day37.java

---
+++ Day37.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class day37 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = input.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println(angka + " adalah bilangan genap.");
+        } else {
+            System.out.println(angka + " adalah bilangan ganjil.");
+        }
+    }
+}
