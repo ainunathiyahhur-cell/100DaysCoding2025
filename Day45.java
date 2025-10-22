86c6e15f1e881561c4aea565b5d805d3d47166aa
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-10-22 22:17:36 +0800

    Update Day45.java

--- Day45.java
+++ Day45.java
@@ -4,29 +4,27 @@ public class day45 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("=== MENU MAKANAN ===");
-        System.out.println("1. Nasi Goreng");
-        System.out.println("2. Mie Ayam");
-        System.out.println("3. Soto Ayam");
-        System.out.println("4. Bakso");
-        System.out.print("Pilih menu (1-4): ");
-        int pilihan = input.nextInt();
+        System.out.print("Masukkan nilai huruf (A-E): ");
+        char nilai = input.next().toUpperCase().charAt(0);
 
-        switch (pilihan) {
-            case 1:
-                System.out.println("Anda memilih Nasi Goreng. Harga: Rp15.000");
+        switch (nilai) {
+            case 'A':
+                System.out.println("Sangat Baik");
                 break;
-            case 2:
-                System.out.println("Anda memilih Mie Ayam. Harga: Rp12.000");
+            case 'B':
+                System.out.println("Baik");
                 break;
-            case 3:
-                System.out.println("Anda memilih Soto Ayam. Harga: Rp14.000");
+            case 'C':
+                System.out.println("Cukup");
                 break;
-            case 4:
-                System.out.println("Anda memilih Bakso. Harga: Rp13.000");
+            case 'D':
+                System.out.println("Kurang");
+                break;
+            case 'E':
+                System.out.println("Sangat Kurang");
                 break;
             default:
-                System.out.println("Pilihan tidak tersedia!");
+                System.out.println("Nilai tidak valid!");
         }
     }
 }
