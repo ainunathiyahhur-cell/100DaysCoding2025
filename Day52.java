f14a2c77ea74b5bd31a2229f0ecccd96ae37fc8a
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2025-10-29 21:17:20 +0800

    Create Day52.java

---
+++ Day52.java
@@ -0,0 +1,12 @@
+public class day52 {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 5) { 
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        }
+
+        System.out.println("Perulangan selesai!");
+    }
+}
